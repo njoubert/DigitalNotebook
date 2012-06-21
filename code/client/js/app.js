996f137d79bca5ff1c01a62ba25a0d06ee4581ff
Niels Joubert <njoubert@gmail.com>
2012-06-20 22:51:13 -0700

    prototype of adding boxes

--- code/client/js/app.js
+++ code/client/js/app.js
@@ -4,10 +4,45 @@ define(
 
 function($) {
 
+  var boxCounter = 0;
+  function genBoxId() {
+    return "DN_box_" + (++boxCounter);
+  }
+  
+  function getNewBox() {
+    var newBox = $("<div>", {id: genBoxId(), class: "Box" });
+    newBox.html();
+    newBox.hover(
+      function() { $(this).addClass("Box_hover"); }, 
+      function() { $(this).removeClass("Box_hover"); });
+    newBox.click(function() { this.contentEditable='true';})
+    return newBox;
+  }
+
+  function initToolbar(divname) {
+    
+    var actions = {
+      '+': function(e) { console.log("new box"); $("#container").append(getNewBox()); },
+      '/': function(e) { console.log("split"); }
+    }
+    
+    $(divname).append("<ul></ul>");
+    var toolbarlist = $(divname + " ul");
+    
+    for (var a in actions) {
+      var el = $("<li>")
+        .html(a)
+        .click(actions[a]);
+      toolbarlist.append(el);
+    }
+    
+  }
+
   function initialize() {
     $().ready(function() {
-      console.log("khaskhsakh");
-      $("#debug").html("LOADED");
+
+      initToolbar("#toolbar");
+      
     });
   }
   
